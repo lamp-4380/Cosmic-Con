e97af78eb6f85ebe67c8b20354147f5694d88f6b
Marques Evandro <evandrocastromarques@gmail.com>
2024-11-07 17:16:15 -0400

    commit-normal

---
+++ Cosmic Con/script.js
@@ -0,0 +1,28 @@
+//CONTADOR
+const targetDate = new Date();
+targetDate.setDate(targetDate.getDate() + 6);
+targetDate.setHours(targetDate.getHours() + 10);
+targetDate.setMinutes(targetDate.getMinutes() + 55);
+targetDate.setSeconds(targetDate.getSeconds() + 14);
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.querySelector(".countdown-container").innerHTML = "<h1>O TEMPO ACABOU!</h1>";
+    }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
